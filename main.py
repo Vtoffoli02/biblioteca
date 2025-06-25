0fb1692c98bce7c5fb19deedfaee14facc66173f
Vtoffoli02 <vanessatoffoli24@gmail.com>
2025-06-24 21:58:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ sys.path.append(os.path.abspath(PROJECT_DIR))
 from db.connection import create_tables
 import services.student_service as service
 
+def show_students():
+    students = service.display_record()
+
+    for student in students:
+        print(f"{student.id} - {student.name} - {student.email} - {student.age}")
+
 #Menu principal
 def main_menu() -> str:
     print("\n Sistema de Cadastro de Alunos")
@@ -30,7 +36,21 @@ if __name__ == "__main__":
             name:str = input("Nome:")
             email:str = input("E-mail:")
             age:int = int(input("Idade:"))
+
             service.create_record(name,email,age)
+            show_students()
+        
+        elif opcao == "2":
+            show_students()
+
+        elif opcao == "3":
+            id:int = int(input("Informe o id do aluno que vc deseja atualizar: "))
+            newName:str = input("Novo nome: ")
+            newEmail:str = input("Novo email: ")
+            newAge:int = int(input("Nova idade: "))
+            print("Aluno atualizado com sucesso")            
+            service.atualizar_alunos
+            show_students()
 
         elif opcao == "5":
             break
--- student_service.py
+++ student_service.py
@@ -1,6 +1,18 @@
 import dao.student_dao as dao 
 from models.student import Student
 
-def create_record(name,email,age):
+def create_record(name:str,email:str,age:int):
     student = Student(name,email,age)
+
+    if age >= 130:
+        print("Erro Idade Incorreta")
+        return
+        
+    #chamada para a camada 3 - 
     dao.insert_student(student)
+
+def display_record():
+    return dao.get_all_students()
+        
+def atualizar_alunos():
+    dao.update()
